6157131b879f5910d435092df8075f99a2f44cae
Julian de Orte <juortegra@gmail.com>
2016-05-17 19:30:11 +0100

    0

---
+++ StandardGame/Assets/BoardGameApi/Actor.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace BoardGameApi
+{
+    public abstract class Actor
+    {
+        static int idCount = 0;
+        public enum types { Cell, Piece };
+        protected int type;
+        private int id;
+        private int name;
+
+        public static int GetCount() { return idCount; }
+        public static void ResetCount() { idCount = 0; }
+
+        public Actor()
+        {
+            id = idCount;
+            idCount++;
+        }
+
+        public int Get_type()
+        {
+            return type;
+        }
+
+        public int GetId()
+        {
+            return id;
+        }
+
+        public int GetName()
+        {
+            return name;
+        }
+
+        public void SetName(int newName)
+        {
+            name = newName;
+        }
+
+        public bool IsActorCell()
+        {
+            if (this.type == (int)Actor.types.Cell)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool IsActorPiece()
+        {
+            if (this.type == (int)Actor.types.Piece)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public Cell TakeActorAsCell(Board board)
+        {
+            if (IsActorPiece())
+            {
+                return board.GetCell((Piece)this);
+            }
+            else
+            {
+                return (Cell)this;
+            }
+        }
+
+    }
+    
+}
