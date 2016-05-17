f4e5da4efcab0cfb4168d8736db4f017f6306619
Julian de Orte <juortegra@gmail.com>
2016-05-17 20:53:09 +0100

    0

---
+++ StandardGame/Assets/Scripts/ActorManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using BoardGameApi;
+
+public class ActorManager
+{
+	public Transform transform;
+	public GameObject go;
+
+	public ActorManager(GameObject go)
+	{
+		this.go = go;
+		this.transform = go.transform;
+	}
+
+
+}
---
+++ StandardGame/Assets/Scripts/Factory_goStandard.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using BoardGameApi;
+
+public class Factory_goStandard : ScriptableObject 
+{
+
+	public enum names {BlackPiece, WhitePiece, Cell};
+
+	public Factory_goStandard()
+	{
+	}
+
+	public GameObject MakeActor(names name, Actor actor, Game game)
+	{
+		Object resource;
+		GameObject newGameObject;
+
+		if (name == names.WhitePiece) 
+		{
+			resource = Resources.Load ("WhitePiece");
+
+			newGameObject = Instantiate (resource) as GameObject;
+
+			newGameObject.transform.GetChild (0).gameObject.AddComponent<go_Cell>().SetSettings(actor, game);
+
+			return newGameObject;
+		}
+
+		if (name == names.BlackPiece) 
+		{
+			resource = Resources.Load ("BlackPiece");
+
+			newGameObject = Instantiate (resource) as GameObject;
+
+			newGameObject.transform.GetChild (0).gameObject.AddComponent<go_Cell>().SetSettings(actor, game);
+
+			return newGameObject;
+		}
+		if (name == names.Cell) 
+		{
+			resource = Resources.Load ("Cell");
+
+			newGameObject = Instantiate (resource) as GameObject;
+
+			newGameObject.transform.GetChild (0).gameObject.AddComponent<go_Cell>().SetSettings(actor, game);
+
+			return newGameObject;
+		}
+
+		return null;
+	}
+
+
+}
